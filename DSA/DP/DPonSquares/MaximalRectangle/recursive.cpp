ad0ffb0ef6f3b9bfe523ff9ca7e0dc23c72bb495
Sweta Gupta <swetagupta1024@gmail.com>
2025-06-02 12:56:55 +0530

    dp on squares

--- DSA/DP/DPonSquares/MaximalRectangle/recursive.cpp
+++ DSA/DP/DPonSquares/MaximalRectangle/recursive.cpp
@@ -2,17 +2,19 @@
 
 using namespace std;
 
-int largestRectangleArea(vector<int> &histo){
-    stack<int> st;
+int largestRectangleArea(vector < int > & histo) {
+    stack < int > st;
     int maxA = 0;
     int n = histo.size();
-    for(int i = 0; i <= n; i++){
-        while(!st.empty() && (i == n || histo[st.top()] >= histo[i])){
+    for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
             int height = histo[st.top()];
             st.pop();
             int width;
-            if(st.empty()) width = i;
-            else width = i - st.top() - 1;
+            if (st.empty())
+                width = i;
+            else
+                width = i - st.top() - 1;
             maxA = max(maxA, width * height);
         }
         st.push(i);
@@ -20,17 +22,30 @@ int largestRectangleArea(vector<int> &histo){
     return maxA;
 }
 
-int countSquares(vector<vector<int>>& matrix) {
-    
+int maximalRectangle(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    int maxArea = 0;
+    vector<int> height(m, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 1) height[j]++;
+            else height[j] = 0;
+        }
+        int area = largestRectangleArea(height);
+        maxArea = max(maxArea, area);
+    }
+    return maxArea;
 }
 
 int main() {
     int m, n;
     cin >> m >> n;
-    vector<vector<int>> v(m, vector<int>(n));
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++) cin >> v[i][j];
+    vector<vector<int>> v(n, vector<int>(m));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++) cin >> v[i][j];
     }
 
-    cout << countSquares(v);
+    cout << maximalRectangle(v);
 }
--- DSA/Practice/check.cpp
+++ DSA/Practice/check.cpp
@@ -1,55 +1,48 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-const int mod = 1000000007;
-
-int f(int i, int j, int isTrue, string &exp) {
-    // Base case 1: If i > j, it's an invalid expression, return 0.
-    if (i > j) return 0;
-    
-    // Base case 2: If i and j are the same, evaluate the single character.
-    if (i == j) {
-        if (isTrue == 1) return exp[i] == 'T' ? 1 : 0;
-        else return exp[i] == 'F' ? 1 : 0;
+int largestRectangleArea(vector < int > & histo) {
+    stack < int > st;
+    int maxA = 0;
+    int n = histo.size();
+    for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+            int height = histo[st.top()];
+            st.pop();
+            int width;
+            if (st.empty())
+                width = i;
+            else
+                width = i - st.top() - 1;
+            maxA = max(maxA, width * height);
+        }
+        st.push(i);
     }
-    
-    ll ways = 0;
-    
-    // Iterate through the expression.
-    for (int ind = i + 1; ind <= j - 1; ind += 2) {
-        ll lT = f(i, ind - 1, 1, exp);  // Number of ways to make the left expression true.
-        ll lF = f(i, ind - 1, 0, exp);  // Number of ways to make the left expression false.
-        ll rT = f(ind + 1, j, 1, exp);  // Number of ways to make the right expression true.
-        ll rF = f(ind + 1, j, 0, exp);  // Number of ways to make the right expression false.
-
-        // Check the operator at the current index and update ways accordingly.
-        if (exp[ind] == '&') {
-            if (isTrue) ways = (ways + (lT * rT) % mod) % mod;
-            else ways = (ways + (lF * rT) % mod + (lT * rF) % mod + (lF * rF) % mod) % mod;
-        }
-        else if (exp[ind] == '|') {
-            if (isTrue) ways = (ways + (lF * rT) % mod + (lT * rF) % mod + (lT * rT) % mod) % mod;
-            else ways = (ways + (lF * rF) % mod) % mod;
-        }
-        else {  // XOR operator
-            if (isTrue) ways = (ways + (lF * rT) % mod + (lT * rF) % mod) % mod;
-            else ways = (ways + (lF * rF) % mod + (lT * rT) % mod) % mod;
-        }
-    }
-    return ways;
+    return maxA;
 }
-
-int evaluateExp(string &exp) {
-    int n = exp.size();
-    return f(0, n - 1, 1, exp);  // Start evaluation with isTrue set to true.
+int maximalAreaOfSubMatrixOfAll1(vector<vector<int>> &mat, int n, int m) {
+    // Write your code here.
+    int maxArea = 0;
+    vector<int> height(m, 0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (mat[i][j] == 1) height[j]++;
+            else height[j] = 0;
+        }
+        int area = largestRectangleArea(height);
+        maxArea = max(maxArea, area);
+    }
+    return maxArea;
 }
 
 int main() {
-    string exp = "F|T^F";
-    int ways = evaluateExp(exp);
-    cout << "The total number of ways: " << ways << "\n";
+
+    vector<vector<int>> mat = {
+        {1, 0, 1, 0, 0}, {1, 0, 1, 1, 1},
+        {1, 1, 1, 1, 1}, {1, 0, 0, 1, 0}
+    };
+    int n = 4, m = 5;
+    int maxArea = maximalAreaOfSubMatrixOfAll1(mat, n, m);
+    cout << "The maximum are is: " << maxArea << "\n";
     return 0;
 }
-
